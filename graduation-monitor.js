1fea8eaba2a7c1ff01ab98769b95fba7f65ccc3e
jimmydv730-crypto <jimmydv730@gmail.com>
2026-05-30 18:21:20 +0530

    Add express server for Render

--- graduation-monitor.js
+++ graduation-monitor.js
@@ -1,3 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Pump Monitor Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 require("dotenv").config();
 
 const axios = require("axios");
